dd243f9901fffab12c79d7fc3fb2f80126a1d3c6
sheapa <55968932+sheapa@users.noreply.github.com>
2020-01-18 16:44:57 -0800

    Created variables in script.js & commented descriptions.

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// My Variables
+
+//Set length
+var passLength = document.querySelector("charInput");
+//Symbols
+var symbols = document.querySelector("symInput");
+//Numbers
+var numbers = document.querySelector("numInput");
+//UperCase
+var lowerCase = document.querySelector("lowInput");
+//LowerCase
+var upperCase = document.querySelector("upInput");
+//Submit
+var submitBtn = document.querySelector("submit");
