dca44dfccb22dd766035c6a35e3874e93974c1b1
sheapa <55968932+sheapa@users.noreply.github.com>
2020-01-18 20:57:56 -0800

    Added my number, special characters, lower case and upper case arrays.

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,21 @@ var lowerCase = document.querySelector("lowInput");
 var upperCase = document.querySelector("upInput");
 //Submit
 var submitBtn = document.querySelector("submit");
+
+//Arrays 
+
+
+//Number Array
+var numNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]; 
+
+//Symbols Array
+var symSyms = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?",
+                    "@","[","\\","]","^","_","`","{","|","}","~"]; 
+               
+//LowerCase Array
+var smallBoys = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+                 "n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+//UpperCase Array               
+var bigBoys = ["A", "B","C","D","E","F","G","H","I","J","K","L","M",
+                 "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
